7098802e7134669a982273a7089e6e0bdbbfb77b
Idaack <Ida_ackehed@hotmail.com>
2022-04-01 13:51:55 +0200

    comments

--- part1_merge_files.py
+++ part1_merge_files.py
@@ -17,9 +17,12 @@ def parse_log_file(log_file_path):
     file_name = os.path.split(log_file_path)[1]
     store_name = os.path.splitext(file_name)[0]
 
+    # Öppna filen med sökvägen log_file_path
     with open(log_file_path) as store:
+        # Skapar en CSV iterator för att i nedan for-loop kunna gå igenom alla rader
         reader = csv.reader(store)
 
+        # Skapar en tom dict som for-loopen fyller allt eftersom med värden. Innehåller id som key i syfte att kunna söka och ta bort canceled-rader. 
         dict = {}
         for line in reader:
             if line[0] == "canceled":    
@@ -37,42 +40,21 @@ def parse_log_file(log_file_path):
                 sales = (time, store_name, total_sek, member, age)
 
                 dict[id] = sales
-        
+        # Funktionen returnerar en lista av värderna i dicten
         return list(dict.values())   
-
-
-    # """
-    # Öppna logfilen och gå igenom alla raderna:
-    #     Om raden startar med "sale," så ska innehållet läggas till i listan av köp.
-    #     Om raden startar med "canceled," så var det något som var fel med köpet, och köpet med samma id ska plockas bort från listan med köp.
-
-    # returnera en lista med en tupel för varje köp.
-    #     Varje tupel ska innehålla: (time, store_name, total_sek, member, age)
-
-    # Hint - Ignorera rader med "cancel" till att börja med om ni kör fast.
-    # Hint - Använd dict för att hålla reda på kopplingen mellan köp-id för att hantera rader med "cancel"
-    # Hint - "123".isdigit() kan användas för att se om en text kan göras om till int
-    # Hint - get_value kan användas för att få ut värden från key=value
-    # """
-
 
 def write_csv_file(csv_file_path, entries):
     """
     Adds all purchases from the list entries as rows in the CSV file csv_file_path.
     The first row in the CSV file is a header with the column names.
     """
-
-    """
-    TODO
-    Öppna CSV-filen med sökvägen csv_file_path för skrivning.
-    Skriv en header-rad med dessa kolumnnamn: 'time', 'store', 'total_sek', 'member', 'age'
-    Skriv en rad för varje köp i listan entries.
-    """
-    
+    # Skapar rubriker 
     header = ["time", "store", "total_sek", "member", "age"]
+    # Öppnar filen, denna gång vill jag kunna skriva i den. 
     with open(csv_file_path, "w", newline="") as csv_file:
         writer = csv.writer(csv_file)
 
+        # Fyller på med min rubrik och mina rader
         writer.writerow(header)
         writer.writerows(entries)    
 
@@ -86,16 +68,14 @@ def merge_log_files_to_csv(log_dir, csv_file_path):
     for file_name in os.listdir(log_dir):
         file_path = os.path.join(log_dir, file_name)
         if os.path.isfile(file_path) and file_name.endswith('.log'):
-            # TODO Läs ut alla entries från logfilen file_path med parse_log_file och lägg till dem på all_entries
-            
+        
+            #Läser ut alla entries och lägger till dem i listan all_entries genom funktionen parse_log_file
             all_entries = all_entries + parse_log_file(file_path)
+    #sorterar listan, default sorterar på första elementet, dvs tiden
     all_entries.sort()
-    # TODO Se till att raderna är sorterade på tid.
-    # Hint: Med tidsformatet som används går det bra att sortera på textsträngar.
-    # Hint: Listor med tupler sorteras på första elementet i tuplerna om inget annat anges.
-    write_csv_file(csv_file_path, all_entries)
-    # TODO Skriv raderna till CSV-filen (csv_file_path) med funktionen write_csv_file
 
+    #Anropar funktionen write_csv_file som skriver över raderna till CSV-filen
+    write_csv_file(csv_file_path, all_entries)
 
 if __name__ == '__main__':
     merge_log_files_to_csv(log_dir='stores/', csv_file_path='output/all-stores.csv')
--- part2_analyze_csv_file.py
+++ part2_analyze_csv_file.py
@@ -18,15 +18,18 @@ def sek_per_purchase(entries):
 
 
 def member_vs_not_member_sum_per_moth(entries):
-    # Entries är en lista av dicts. 
-    # {"key=butiken":values=dict.{key=månad:values=[total summa, antal poster]}}
-    
+
+    # Entries är en lista av dicts.
+    # Skapar en dict-dict-lista i syfte att kunna fylla på stores med mina värden.
+    # {"key=butiken":values=dict.{key=månad:values=[total summa, antal poster]}} 
     stores = {"store1": {1:[0,0], 2:[0,0], 3:[0,0], 4:[0,0], 5:[0,0], 6:[0,0]}, \
             "store2": {1:[0,0], 2:[0,0], 3:[0,0], 4:[0,0], 5:[0,0], 6:[0,0]}, \
             "store3": {1:[0,0], 2:[0,0], 3:[0,0], 4:[0,0], 5:[0,0], 6:[0,0]}, \
             "store4": {1:[0,0], 2:[0,0], 3:[0,0], 4:[0,0], 5:[0,0], 6:[0,0]}}
 
+    # Skapar en loop för att gå igenom varje rad i entries
     for item in entries:
+        # Hämtar ut värden och benämner de 
         month = item["time"].month
         store = item["store"]
         value = item["total_sek"]
@@ -36,40 +39,32 @@ def member_vs_not_member_sum_per_moth(entries):
         # Antal poster
         stores[store][month][1] = stores[store][month][1] + 1
     
-        
+    # Skapar två listor för att kunna gå igenom dessa värden i en loop i syfte att matcha keys mot values i dicts.     
     store_names = ['store1','store2','store3','store4']
     month_numbers = [1,2,3,4,5,6]
 
-    for store in store_names:
-        for month in month_numbers:
-            mean = stores[store][month][0]/stores[store][month][1] if stores[store][month][1] != 0 else 0
-            print(f'For {store} and month {month} mean is {mean:.2f}')
-
-    
-    """
-    TODO
-    Räkna ut och skriv ut genomsnittlig total försäljning per affär och månad.
-    Räkna ut och skriv ut separat för medlemmar och icke medlemmar.
-
-    Det är ok att hårdkoda in att tidsintervallet sträcker sig över 6 månader och det finns 4 affärer.
-    """
-
-    #print(f'Members: {res_members:.2f} sek/month/store')
-    #print(f'Not members: {res_not_members:.2f} sek/month/store')
-
+    #Loopa igenom varje butik, och för varje butik loopa igenom de månader som finns. 
+    for s in store_names:
+        for m in month_numbers:
+            #Månad 6 har inga värden, därför behövs en if-sats.
+            mean = stores[s][m][0]/stores[s][m][1] if stores[s][m][1] != 0 else 0
+            print(f'For {s} and month {m} mean is {mean:.2f}')
 
 def members_mean_age_per_hour(entries):
     print('\nMean member customer age per open hour:')
 
+    # Skapar en dict med en lista för att kunna fylla på värden.
     hours = {7:[0,0],8: [0,0],9: [0,0],10: [0,0],11: [0,0],12: [0,0],13: [0,0],14: [0,0],15: [0,0],16: [0,0],17:[0,0],18:[0,0],19:[0,0],20: [0,0],21: [0,0],22: [0,0]}
 
+    # Skapar en loop för att gå igenom varje rad i entries
     for item in entries:
 
+        # Hämtar ut värden och benämner de 
         member = item["member"]
         hour = item["time"].hour
         age = item["age"]
         
-        
+        # Kolla endast de rader som är en medlem och som handlat mellan 7-22
         if member == True and 7 <= hour and hour <= 22 :
                       
             # Summan
@@ -77,21 +72,13 @@ def members_mean_age_per_hour(entries):
             # Antal poster
             hours[hour][1] = hours[hour][1] + 1
 
+    # Skapar en lista för att kunna gå igenom dessa värden i en loop i syfte att matcha keys mot values i dicts.   
     open_hours = [7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
 
+    # Gå igenom listan open_hours och hämta värdena i list-delen på plats 0 och 1. Räkna ut genomsnittet genom dessa värden. 
     for h in open_hours:
         mean = hours[h][0]/hours[h][1]
         print(f'hour={h} mean_age={mean:.1f}')      
-        
-    """
-    TODO
-    Affärerna är öppna mellan 7 och 22.
-    Skriv ut en rad för varje öppen timme, där varje rad innehåller timme och genomsnittsåldern på medlemmar som handlade denna timme.
-
-    Skriv ut på det här formatet:
-    print(f'hour={hour} mean_age={mean_age:.1f}')
-    """
-
 
 def read_csv_file(csv_file_path):
     import csv
