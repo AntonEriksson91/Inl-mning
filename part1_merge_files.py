8f4df346afca32cb2561379e273eb8ce7bf64f35
Idaack <Ida_ackehed@hotmail.com>
2022-04-01 14:24:36 +0200

    comments2

--- part1_merge_files.py
+++ part1_merge_files.py
@@ -25,11 +25,14 @@ def parse_log_file(log_file_path):
         # Skapar en tom dict som for-loopen fyller allt eftersom med värden. Innehåller id som key i syfte att kunna söka och ta bort canceled-rader. 
         dict = {}
         for line in reader:
-            if line[0] == "canceled":    
+            # Första kolumen avgör om ordern är canceled eller inte.
+            if line[0] == "canceled":
+                # Tar ut id ur den cancelerade ordern   
                 id = get_value(line[1])
-                
+                # Tar bort ordern från dicten
                 del dict[id]
             else:
+                # Tar ut värden som ska fylla tuplen sales
                 id = get_value(line[1])
                 time = get_value(line[2])
                 total_sek = get_value(line[3])
@@ -37,8 +40,9 @@ def parse_log_file(log_file_path):
                 birth_year = get_value(line[5])
                 age = 2022 - int(birth_year) if birth_year.isdigit() else "na"
 
+                # Skapar en tupel
                 sales = (time, store_name, total_sek, member, age)
-
+                # I dict läggs id in som key och sales-tupeln som värde
                 dict[id] = sales
         # Funktionen returnerar en lista av värderna i dicten
         return list(dict.values())   
@@ -50,11 +54,11 @@ def write_csv_file(csv_file_path, entries):
     """
     # Skapar rubriker 
     header = ["time", "store", "total_sek", "member", "age"]
-    # Öppnar filen, denna gång vill jag kunna skriva i den. 
+    # Öppnar filen och gör det möjligt att skriva i den
     with open(csv_file_path, "w", newline="") as csv_file:
         writer = csv.writer(csv_file)
 
-        # Fyller på med min rubrik och mina rader
+        # Fyller på med rubrik och rader
         writer.writerow(header)
         writer.writerows(entries)    
 
@@ -62,19 +66,21 @@ def merge_log_files_to_csv(log_dir, csv_file_path):
     """
     Read and parse all store log files in log_dir, and then write all entries to a CSV file at csv_file_path.
     """
-    
+    # Skapar en tom lista
     all_entries = []
     
+    # Gå igenom alla filer i mappen log_dir
     for file_name in os.listdir(log_dir):
         file_path = os.path.join(log_dir, file_name)
+        # Kontrollerar så att det är en log-fil
         if os.path.isfile(file_path) and file_name.endswith('.log'):
         
-            #Läser ut alla entries och lägger till dem i listan all_entries genom funktionen parse_log_file
+            # Fyller på listan med alla tuples från funktionen parse_log_file för nuvarande fil
             all_entries = all_entries + parse_log_file(file_path)
-    #sorterar listan, default sorterar på första elementet, dvs tiden
+    # Sorterar listan, default sorterar på första elementet, dvs tiden
     all_entries.sort()
 
-    #Anropar funktionen write_csv_file som skriver över raderna till CSV-filen
+    #Anropar funktionen write_csv_file som skriver över raderna i all_entries till CSV-filen
     write_csv_file(csv_file_path, all_entries)
 
 if __name__ == '__main__':
